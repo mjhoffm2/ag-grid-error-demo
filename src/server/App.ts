54499f4df7a9aadf6b931732832f25b279457b28
Matt Hoffman <matt.hoffman@echelon.org>
2019-01-15 13:34:57 -0600

    initial commit

---
+++ src/server/App.ts
@@ -0,0 +1,73 @@
+import * as express from "express";
+import * as path from "path";
+import * as webpack from 'webpack';
+import * as devMiddleware from 'webpack-dev-middleware';
+import * as hotMiddleware from 'webpack-hot-middleware';
+import {Configuration} from "webpack";
+
+import * as compression from 'compression';
+
+const configs: (env: any, options: any) => Configuration[] = require('../../webpack.config');
+const config = configs({}, {mode: "development"}).find(config => config.name === "web");
+
+export class App {
+    // ref to Express instance
+    public express: express.Application;
+    //Run configuration methods on the Express instance.
+    constructor() {
+        this.express = express();
+
+        this.express.use(compression());
+
+        this.configureMiddleWare();
+
+        //configure endpoints that are handled by api calls
+        this.configureApi();
+
+        //static files
+        this.express.use(express.static(path.join(__dirname, '../../public')));
+
+        //serve static home page for all remaining requests
+        this.express.get("*", (req, res, next) => {
+            if(req.path.lastIndexOf('.') > req.path.lastIndexOf('/')) {
+                //if this request looks like a static file, ignore it
+                return next();
+            }
+
+            let filePath: string;
+
+            if(process.env.NODE_ENV === 'development') {
+                filePath = path.resolve(__dirname, '../devmain.html');
+            } else {
+                filePath = path.resolve(__dirname, '../../public/build/main.html');
+            }
+            res.sendFile(filePath);
+        });
+    }
+
+    // Configure Express middleware.
+    private configureMiddleWare(): void {
+
+        if(process.env.NODE_ENV === 'development') {
+
+            const compiler = webpack({
+                ...config,
+                mode: "development",
+            });
+            //const compiler = webpack(configs);
+
+            //enable hot module replacement part 1
+            this.express.use(devMiddleware(compiler, {
+                publicPath: config!.output!.publicPath!
+            }));
+
+            //enable hot module replacement part 1
+            this.express.use(hotMiddleware(compiler));
+        }
+    }
+
+    // Configure API endpoints.
+    private configureApi(): void {
+
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var config = (env, options) => [
+    //web configuration
+    {
+        name: 'web',
+        entry: options.mode === 'production' ? [
+            './src/web/boot-client.tsx'
+        ] : [
+            //we configure the hot reloader as a separate entry point to the application
+            'webpack-hot-middleware/client',
+
+            './src/web/boot-client.tsx'
+        ],
+        output: {
+            path: path.resolve(__dirname, './public/build'),
+            publicPath: '/build/',
+            filename: 'bundle.js',
+        },
+        resolve: {
+            //automatically infer '.ts' and '.tsx' when importing files
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test:/\.css$/,
+                    use: options.mode === 'production' ?
+                        [MiniCssExtractPlugin.loader, 'css-loader'] :
+                        ['style-loader', 'css-loader'],
+                },
+                {
+                    test:/\.tsx?$/,
+                    include: path.resolve(__dirname, "./src/web/"),
+                    loader: "awesome-typescript-loader"
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg|ttf|otf|woff|woff2|eot)$/,
+                    loader: 'url-loader?limit=4096'
+                }
+            ]
+        },
+
+        //see https://webpack.js.org/configuration/devtool/ for options
+        devtool: options.mode === 'production' ? "source-map" : "cheap-module-eval-source-map",
+
+        plugins: options.mode === 'production' ? [
+            new MiniCssExtractPlugin(),
+            new HtmlWebpackPlugin({
+                filename: 'main.html',
+                hash: true,
+                template: './src/template.html'
+            })
+        ] : [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    },
+
+    //server configuration
+    {
+        name: 'server',
+        entry: ['./src/server/main.ts'],
+        target: 'node',
+        externals: [nodeExternals()],
+        output: {
+            path: path.resolve(__dirname, './build'),
+            filename: '[name].js',
+        },
+        resolve: {
+            //automatically infer '.ts' and '.tsx' when importing files
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        module: {
+            rules: [
+                {
+                    test:/\.tsx?$/,
+                    include: path.resolve(__dirname, "./src/server/"),
+                    loader: "awesome-typescript-loader"
+                }
+            ]
+        },
+
+        //by default, webpack will set these to '/', so override that behavior on the server
+        node: {
+            //setting these to 'true' causes them to keep the same values relative to the source code
+            //setting these to 'false' would cause them to have values relative to the output code
+            __dirname: true,
+            __filename: true
+        }
+    }
+];
+module.exports = config;
